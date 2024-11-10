aaaffdb5dd60eee4914720c891d98f2e66e33963
Akshit203 <akshitgupta2003.ak@gmail.com>
2024-11-10 19:43:57 +0530

    .

--- 03_Tailwind_Props/src/main.jsx
+++ 03_Tailwind_Props/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Demo from './Components/demo.jsx';
+import Demo from './Components/Demo.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
