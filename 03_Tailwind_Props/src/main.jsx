8321a7bf117d92118585a6b5fdc4ffed00b74945
Akshit203 <akshitgupta2003.ak@gmail.com>
2024-11-10 19:37:27 +0530

    .

--- 03_Tailwind_Props/src/main.jsx
+++ 03_Tailwind_Props/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Demo from './Components/Demo.jsx'
+import Demo from './Components/demo.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,11 +10,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
     <div className='flex flex-row flex-wrap'>
       
-    <Demo name1 = "Jacket" price = "$24" avl = "In stock" img1 = "\src\Images\Jacket.jpg"/>
-    <Demo name1 = "T-Shirts" price = "$4" avl = "Out of stock" img1 = "\src\Images\T-shirts.jpg"/>
-    <Demo name1 = "Jeans" price = "$12" avl = "In stock" img1 = "\src\Images\Jeans1.jpeg"/>
-    <Demo name1 = "T-Shirts" price = "$4" avl = "Out of stock" img1 = "\src\Images\T-shirts.jpg"/>
-    <Demo name1 = "Jacket" price = "$24" avl = "In stock" img1 = "\src\Images\Jacket.jpg"/>
+    <Demo name1 = "Jacket" price = "$24" avl = "In stock" img1 = "/Jacket.jpg"/>
+    <Demo name1 = "T-Shirts" price = "$4" avl = "Out of stock" img1 = "/T-shirts.jpg"/>
+    <Demo name1 = "Jeans" price = "$12" avl = "In stock" img1 = "/Jeans1.jpeg"/>
+    <Demo name1 = "T-Shirts" price = "$4" avl = "Out of stock" img1 = "/T-shirts.jpg"/>
+    <Demo name1 = "Jacket" price = "$24" avl = "In stock" img1 = "/Jacket.jpg"/>
 
     </div>
 
